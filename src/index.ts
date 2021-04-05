21dd20562e011ca58b750013c71680ebdc80c794
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-05 21:15:37 +0300

    add: basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'hello from express' });
+});
+
+app.listen(4000, () => {
+  console.log(`http://localhost:4000`);
+});
